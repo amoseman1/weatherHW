fb96f4a2d7cdcab8a268d9e932f0764f3dd47db0
Waterbug <amoseman18@gmail.com>
2020-11-05 10:06:54 -0700

    fixed local storage and cityList bugs

--- script.js
+++ script.js
@@ -4,46 +4,62 @@ var buttonEl = document.getElementById("searchBtn");
 var cityEl = document.getElementById("cityList");
 var currentDayEl = document.getElementById("dayWeather");
 var dateEL = document.getElementById("date");
+//retrieves value of the key 'cities' and stores empty array in searchList variable
+var searchList = JSON.parse(localStorage.getItem('cities')) || []
 
 //created value from the users input text as a global variable, used as a parameter, that will be passed through the API call function below
 var formSubmitHandler = function (event) {
   event.preventDefault();
-  var city = formInputEl.value.trim(); 
-  if (city === "") { alert("must be a valid entry");
-  return };    // makes it so you can't search for an empty text and doesn't store empty value in local storage
-  $("#userCityInput").val(" ");   // clears out the input box
+  var city = formInputEl.value.trim();
+  if (city === "" || city === undefined) { //conditional so you can't search for an empty text and doesn't store empty value in local storage
+    alert("must be a valid entry");
+  };
 
+  if (searchList.includes(city)) { //conditional so you don't push the same city to the local storage
+    console.log("city already exsists")
+  } else {
+    searchList.push(city)
+  };
+
+  $("#userCityInput").val(" ");   // clears out the input box
   getDayRepo(city);        //calls the function
-  getForecastRepo(city);   
-  createCityList(city);
+  getForecastRepo(city);
+  createCityList();
 
   $("#dayWeather").empty(); // clears the previous city's data
   $("#forecast .row").empty();
 }
-
+//globally calling function
+createCityList();
 //event handler added to button element that calls functions above
 $("#searchBtn").on("click", formSubmitHandler);
 
-function createCityList(city) {
-  //retrieves value of the key 'cities' and stores empty array in searchList variable
-  var searchList = JSON.parse(localStorage.getItem('cities')) || []
-  //pushes user input into array
-  searchList.push(city); 
+function createCityList() {
   $("#cityList").empty();
   // loops through array and dynamically creates element for each array item
   for (var i = 0; i < searchList.length; i++) {
-    console.log(searchList[i]);  
+    console.log(searchList[i]);
     //we are storing the stringified value of searchList into the key of cities
     localStorage.setItem("cities", JSON.stringify(searchList));
-    var pastCitiesBtn = $("<li>") 
-    .addClass("list-group-item cityBtn")
-    .text(searchList[i]);
-    $("#cityList").append(pastCitiesBtn);}
+    var pastCitiesBtn = $("<li>")
+      .addClass("list-group-item cityBtn")
+      .text(searchList[i]);
+    $("#cityList").append(pastCitiesBtn);
+  }
 }
+
+//onclick event to retrieve citys data from cityList elements
+$("#cityList").on("click", "li", function () {
+  var searchInput = $(this).text();
+  getDayRepo(searchInput);
+  getForecastRepo(searchInput);
+  console.log("testing");
+  $("#dayWeather").empty();
+  $("#forecast .row").empty();
+})
 
 //function nests two api calls and dynamically creates elements to store specific data
 function getDayRepo(city) {
-  
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=9215ad3aaa6f9b3960ceccb94240987d&units=imperial";
   fetch(requestUrl)
@@ -67,7 +83,7 @@ function getDayRepo(city) {
       $("#dayWeather").append(tempData);
       var humidityData = $("<div>")
         .addClass("card-text")
-        .text("Humidity:" + " " + data.main.humidity + " %"); 
+        .text("Humidity:" + " " + data.main.humidity + " %");
       $("#dayWeather").append(humidityData);
       var windData = $("<div>")
         .addClass("card-text")
@@ -77,9 +93,9 @@ function getDayRepo(city) {
       //variables that grab data from user input and inserts them into the api call below for a city's uv index
       var cityLat = data.coord.lat;
       var cityLon = data.coord.lon
-      var requestUvUrl =    
+      var requestUvUrl =
         "https://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial" + "&exclude=minutely,hourly,alerts" + "&appid=9215ad3aaa6f9b3960ceccb94240987d"
-        fetch(requestUvUrl)
+      fetch(requestUvUrl)
         .then(function (response) {
           return response.json();
         })
@@ -89,35 +105,34 @@ function getDayRepo(city) {
           //conditional statement to change the class of the uvindex (and color) based off index data value
           var uvIndex = data.value
           var uvIndexStatus = $('<span>').text(uvIndex);
-             if (uvIndex <= 2) {
-              uvIndexStatus.addClass("badge badge-success");
-            } else if (uvIndex > 2 && uvIndex <= 5) {
-              uvIndexStatus.addClass("badge badge-warning");
-            } else {
-              uvIndexStatus.addClass("badge badge-danger");
-            }
-            uvData.append(uvIndexStatus)
-            $('#dayWeather').append(uvData)
+          if (uvIndex <= 2) {
+            uvIndexStatus.addClass("badge badge-success");
+          } else if (uvIndex > 2 && uvIndex <= 5) {
+            uvIndexStatus.addClass("badge badge-warning");
+          } else {
+            uvIndexStatus.addClass("badge badge-danger");
+          }
+          uvData.append(uvIndexStatus)
+          $('#dayWeather').append(uvData)
         });
     });
 }
 
 //function uses api call for a specific city and with a loop dynamically creates 5 card elements to append the data to 
 function getForecastRepo(city) {
-  
-  var requestForecastUrl =                        
+  var requestForecastUrl =
     "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=9215ad3aaa6f9b3960ceccb94240987d";
-    fetch(requestForecastUrl)
+  fetch(requestForecastUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
       for (var i = 0; i < data.list.length; i++) {
-       if (data.list[i].dt_txt.indexOf("9:00:00") !== -1) {
+        if (data.list[i].dt_txt.indexOf("9:00:00") !== -1) {
           var fiveDays = $("<div>")
             .addClass("card-text")
-            .text(new Date("date:" + data.list[i].dt_txt).toLocaleDateString());  
+            .text(new Date("date:" + data.list[i].dt_txt).toLocaleDateString());
           var column = $("<div>").addClass("col-md-2");
           var card = $("<div>").addClass("card bg-primary text-white");
           var tempFiveDay = $("<p>")
@@ -127,10 +142,10 @@ function getForecastRepo(city) {
             .addClass("card-text")
             .text("Humidity:" + " " + data.list[i].main.humidity + "%");
           var imgFiveDay = $("<img>").attr(
-              "src",
-              "http://openweathermap.org/img/wn/" +
-                data.list[i].weather[0].icon +
-                "@2x.png");  
+            "src",
+            "http://openweathermap.org/img/wn/" +
+            data.list[i].weather[0].icon +
+            "@2x.png");
           column.append(card.append(fiveDays, tempFiveDay, humidityFiveDay, imgFiveDay));
           $("#forecast .row").append(column);
         }
